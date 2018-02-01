10ffa4991ec9c7203521d48e1dd6185c69c59b88
Satyaki Sikdar <ssikdar@nd.edu>
2018-02-01 01:12:22 -0500

    added variable LIM to upper bound the size of buckets
    originally set to 2000.

--- graph_parser.py
+++ graph_parser.py
@@ -13,6 +13,7 @@ import bintrees
 
 verbose = 1
 
+LIM = 10000
 
 class UnificationFailure(Exception):
     pass
@@ -27,7 +28,7 @@ class Agenda(object):
         return len(self.agenda)
 
     def add(self, d, pri=1.):
-        if len(self.agenda) > 2000:# and self.agenda.min_key()[0] < pri:
+        if len(self.agenda) > LIM:# and self.agenda.min_key()[0] < pri:
             #print("Added ", pri, ": ", self.agenda[-1][0], self.agenda[0][0])
 
             self.agenda.insert((pri,d), d)
